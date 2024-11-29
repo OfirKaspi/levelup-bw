eb5694c9b43f311801c94da3be9266b2520e2211
Ofir Kaspi <kaspiofir@gmail.com>
2024-11-29 21:53:28 +0200

    responsive nav bar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,23 +28,17 @@ const Hero = () => {
           {/* <p className="p-5">For startups, enterprise leaders, media & publishers, and social good. </p> */}
         </div>
         <div className="space-x-3">
-          <div className="relative inline-flex  group">
-            <div
-              className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
-            </div>
-            <a href="#" title="Contact Us"
-              className="relative inline-flex items-center justify-center px-6 py-3 text- font-bold text-white transition-all duration-200 bg-gray-900 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-              role="button">Contact Us
-            </a>
+          <div className="relative inline-flex group">
+            <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt" />
+            <button className="relative h-10 px-4 text-white transition-all duration-200 bg-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
+              Contact Us
+            </button>
           </div>
-          <div className="relative inline-flex  group">
-            <div
-              className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
-            </div>
-            <a href="#" title="Contact Us"
-              className="relative inline-flex items-center justify-center px-6 py-3 text- font-bold text-gray-900 transition-all duration-200 bg-white rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-              role="button">Our Works
-            </a>
+          <div className="relative inline-flex group">
+            <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt" />
+            <button className="relative h-10 px-4 text-gray-900 transition-all duration-200 bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
+              Our Works
+            </button>
           </div>
         </div>
       </section>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,46 +7,69 @@ import { Separator } from "./ui/separator";
 import Logo from "./Logo";
 import { usePathname } from "next/navigation";
 import { links } from "@/utils/links";
+import useResponsive from "@/hooks/useResponsive";
 
 
 const Navbar = () => {
   const pathname = usePathname()
   const isHomePage = pathname === "/"
+  const { isMobile } = useResponsive()
 
   return (
-    <div className="w-full p-5 z-10">
-      <nav className="flex items-center justify-between">
-        {/* Logo  */}
-        <Logo isTextWhite={isHomePage} />
+    <nav className="w-full p-5 z-10 flex items-center justify-between">
+      {/* Logo  */}
+      <Logo isTextWhite={isHomePage} />
 
-        {/* menu  */}
+      {isMobile ? (
         <Sheet>
           <SheetTrigger>
-            {/* <div className="w-10 h-10 bg-slate-400 flex items-center justify-center rounded-md"> */}
             <Menu className="bg-slate-300 text-purple-800 h-10 w-10 p-2 rounded-md" />
-            {/* </div> */}
           </SheetTrigger>
           <SheetContent >
-            <SheetTitle className="text-purple-800">
+            <SheetTitle className="text-purple-800 pt-5">
               Welcome to LevelUp!
             </SheetTitle>
             <Separator className="my-5" />
             <SheetDescription asChild >
-              <ul className="space-y-2">
-                {links.map((link) => (
-                  <li key={link.href}>
-                    <Link href={link.href} className={`flex gap-2 items-center text-base ${pathname === link.href && 'text-purple-800'}`}>
+              <div className="space-y-5">
+                <ul className="space-y-5">
+                  {links.map((link) => (
+                    <Link role="list" key={link.href} href={link.href} className={`flex gap-2 items-center text-base ${pathname === link.href && 'text-purple-800'}`}>
                       {link.icon}
                       {link.text}
                     </Link>
-                  </li>
-                ))}
-              </ul>
+                  ))}
+                </ul>
+                <Separator />
+                <div className="relative inline-flex group">
+                  <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt" />
+                  <button className="relative h-10 px-4 text-white transition-all duration-200 bg-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
+                    Contact Us
+                  </button>
+                </div>
+              </div>
             </SheetDescription>
           </SheetContent>
         </Sheet>
-      </nav>
-    </div>
+      ) : (
+        <>
+          <ul className="flex gap-2">
+            {links.map((link) => (
+              <Link role="list" key={link.href} href={link.href} className={`flex items-center justify-center h-10 px-4 rounded-md transition-all ${isHomePage && 'text-white'} ${pathname === link.href ? 'bg-gray-900 text-white' : 'hover:bg-purple-800 hover:text-white'}`}>
+                {link.text}
+              </Link>
+            ))}
+          </ul>
+
+          <div className="relative inline-flex group">
+            <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt" />
+            <button className="relative h-10 px-4 text-white transition-all duration-200 bg-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
+              Contact Us
+            </button>
+          </div>
+        </>
+      )}
+    </nav>
   );
 }
 
