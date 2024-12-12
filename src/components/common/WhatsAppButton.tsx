3af2418e6d7c8809440da0b0f0c11c8678d24a02
Ofir Kaspi <kaspiofir@gmail.com>
2024-12-12 16:24:19 +0200

    whats app button optimization

--- src/components/common/WhatsAppButton.tsx
+++ src/components/common/WhatsAppButton.tsx
@@ -1,15 +1,14 @@
-import { CONFIG } from '@/lib/config';
 import Image from 'next/image';
 import Link from 'next/link';
+import { CONFIG } from '@/lib/config';
 
 const WhatsAppButton = () => {
   const phoneNumber = CONFIG.WHATSAPP_NUMBER
-  console.log(phoneNumber);
   const whatsappUrl = `https://wa.me/${phoneNumber}`;
 
   return (
     <Link href={whatsappUrl} target="_blank" rel="noopener noreferrer">
-      <div className="fixed bottom-4 right-4 bg-green-500 rounded-full p-2 shadow-lg hover:bg-green-600 transition">
+      <div className="fixed bottom-4 right-4 bg-green-500 rounded-full p-3 shadow-lg hover:bg-green-600 transition">
         <Image
           src={`/socialMedia/whatsapp.svg`}
           alt={`whatsapp logo`}
